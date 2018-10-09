7b0126c70ec0740a21caf7c2d43caf9ef7367b62
William Neild <wneild5@gmail.com>
2018-10-09 07:15:53 +0100

    Added Music Functions

---
+++ wms/music/music.py
@@ -0,0 +1,34 @@
+
+
+class Music:
+    def __init__(self):
+        pass
+    
+    # Albums
+    def getAlbum(self, id):
+        pass
+    
+    def getAllAlbums(self):
+        pass
+
+    # Artists
+    def getArtist(self, id):
+        pass
+    
+    def getAllArtists(self):
+        pass
+    
+    # Genres
+    def getGenre(self, id):
+        pass
+    
+    def getAllGenres(self):
+        pass
+
+    # Songs
+    def getSong(self, id):
+        pass
+    
+    def getAllSongs(self):
+        pass
+
