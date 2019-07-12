8aab9d23dc913ccad45764064d6a78f8f1ac9ede
William Neild <wneild5@gmail.com>
2019-07-12 14:04:18 +0100

    add test for graphql including songs

--- index.js
+++ index.js
@@ -1,2 +1,10 @@
+/**
+|--------------------------------------------------
+| Wills Media Server
+| index.js
+| by wsngamerz
+|--------------------------------------------------
+*/
+
 require = require("esm")(module)
 module.exports = require("./main.js")
--- main.js
+++ main.js
@@ -1,12 +1,85 @@
+/**
+|--------------------------------------------------
+| Wills Media Server
+| main.js
+| by wsngamerz
+|--------------------------------------------------
+*/
+
 import express from "express"
 import bodyParser from "body-parser"
+import graphqlHTTP from "express-graphql"
+import { buildSchema } from "graphql"
 
+
+let songs = [
+    {
+        "_id": Math.random().toString(),
+        "name": "I Don't Care",
+        "artist": "Ed Sheeran & Justin Bieber"
+    },
+    {
+        "_id": Math.random().toString(),
+        "name": "Viva la Vida",
+        "artist": "Coldplay"
+    },
+    {
+        "_id": Math.random().toString(),
+        "name": "Sail",
+        "artist": "AWOLNATION"
+    }
+] 
+
+// Express app
 const app = express()
 
 app.use(bodyParser.json())
 
-app.get("/", (req, res, next) => {
-    res.send("Hello World")
-})
+// GraphQL Middleware
+app.use("/api", graphqlHTTP({
+    schema: buildSchema(`
+        type Song {
+            _id: ID!
+            name: String!
+            artist: String!
+        }
 
+        input SongInput {
+            name: String!
+            artist: String!
+        }
+
+        type RootQuery {
+            songs: [Song!]!
+        }
+
+        type RootMutation {
+            createSong(songInput: SongInput): Song
+        }
+
+        schema {
+            query: RootQuery
+            mutation: RootMutation
+        }
+    `),
+    rootValue: {
+        songs: () => {
+            return songs
+        },
+        createSong: (args) => {
+            const newSong = {
+                "_id": Math.random().toString(),
+                "name": args.songInput.name,
+                "artist": args.songInput.artist
+            }
+            songs.push(newSong)
+            return newSong
+        }
+    },
+    graphiql: true
+}))
+
+app.get("/", (req, res, next) => res.send("Hello World"))
+
+// Listen on port
 app.listen(3000)
